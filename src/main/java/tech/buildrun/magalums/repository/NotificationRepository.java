5f514b78bba8f5229a63b0a60dc146c5e06fa580
Marcelly Guimarães <souzaguimaraesarcelly@gmail.com>
2025-01-06 20:36:14 -0300

    finalizado

--- src/main/java/tech/buildrun/magalums/repository/NotificationRepository.java
+++ src/main/java/tech/buildrun/magalums/repository/NotificationRepository.java
@@ -1,9 +1,13 @@
 package tech.buildrun.magalums.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import tech.buildrun.magalums.entity.Notification;
+import tech.buildrun.magalums.entity.Status;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-
+    List<Notification> findByStatusInAndDateTimeBefore(List<Status> Status, LocalDateTime dateTime);
 }
--- src/main/java/tech/buildrun/magalums/service/NotificationService.java
+++ src/main/java/tech/buildrun/magalums/service/NotificationService.java
@@ -5,8 +5,10 @@ import tech.buildrun.magalums.entity.Notification;
 import tech.buildrun.magalums.entity.Status;
 import tech.buildrun.magalums.repository.NotificationRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 import org.springframework.stereotype.Service;
 
@@ -37,6 +39,23 @@ public class NotificationService {
             n.setStatus(Status.Values.CANCELED.toStatus());
             notificationRepository.save(n);
         });
+    }
+
+    public void checkAndSend(LocalDateTime dateTime) {
+        var notifications = notificationRepository.findByStatusInAndDateTimeBefore(List.of(
+                Status.Values.PENDING.toStatus(),
+                Status.Values.ERROR.toStatus()), dateTime);
+
+        notifications.forEach(sendNotification());
+    }
+
+    private Consumer<Notification> sendNotification() {
+        return n -> {
+            // TODO - realizar o envio da notificação
+
+            n.setStatus(Status.Values.SUCCESS.toStatus());
+            notificationRepository.save(n);
+        };
     }
 
     // public void cancelNotification(Long notificationId) {
